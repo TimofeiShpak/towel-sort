cbb5951c0ce978e3e143e43b49a0d2f17b635c49
TimofeiShpak <60565887+TimofeiShpak@users.noreply.github.com>
2021-02-01 10:14:01 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-module.exports = function towelSort (matrix) {
-  if(!Array.isArray(matrix))return [];
-  let arr = [];
-  let j=0;
-  let bool=1;
-  let repeat=1;
-  for(let i=0; i<matrix.length; i++){
-  	if(!Array.isArray(matrix[i])){arr[j]=matrix[i];j++;}
-  		else{
-      if(i%2==0)matrix[i].sort((a,b)=>  a-b);
-      else matrix[i].sort((a,b)=>  b-a);
-  			for(let q=0; q<matrix[i].length; q++){
-  				if(!Array.isArray(matrix[i][q])){arr[j]=matrix[i][q];j++;}
-  					else{ 
-  						for(let w=0; w<matrix[i][q].length; w++){
-  							if(!Array.isArray(matrix[i][q][w])){arr[j]=matrix[i][q][w];j++;}
-  						}
-  					}
-			}
-		}
-		repeat=1;
+function turnArr(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i % 2 !== 0) {
+      newArr.push(arr[i].reverse());
+    } else {
+      newArr.push(arr[i]);
+    }
   }
-  return arr;
+  return newArr;
+}
+
+module.exports = function towelSort (matrix) {
+  let result = [];
+  if (matrix) {
+    matrix = turnArr(matrix);
+    result = [].concat(...matrix);
+    while(Array.isArray(result[0])) {
+      result = turnArr(result);
+      let newArray = [].concat(...result);
+      result = newArray;
+    }
+  }
+  return result;
 }
